d7eeffb71bfdc395b0fca778bd96cd4039cebb0e
kristerduster <chenka3@uci.edu>
2024-05-25 16:04:04 -0700

    optimize compiler and sorter

--- Sorter.py
+++ Sorter.py
@@ -11,27 +11,58 @@ class Sorter:
         self.prereq_freq_dict = prereq_freq_dict
     
     def get_avg_score(self):
-        for index, row in self.sorted_lectures_df.iterrows():
-            course = row["course"]
-            instructor = row["instructors"][0]
-            score = 0
-            div = 0
-            if "avg gpa" in self.sorting_parameters and instructor in self.gpa_dict.keys():
-                score += self.gpa_dict[instructor] 
-                div += 1
-            if "avg rating" in self.sorting_parameters and instructor in self.rating_dict.keys():
-                score += self.rating_dict[instructor]
-                div += 1
-            if "avg difficulty" in self.sorting_parameters and instructor in self.difficulty_dict.keys():
-                score -= self.difficulty_dict[instructor]
-                div += 1
-            if "would take again" in self.sorting_parameters and instructor in self.would_take_again_dict.keys():
-                score += self.would_take_again_dict[instructor]
-                div += 1
-            if "prereq frequency" in self.sorting_parameters and course in self.prereq_freq_dict.keys():
-                score += self.prereq_freq_dict[course]
-                div += 1
-            self.sorted_lectures_df.at[index, "score"] = score/div
+        self.sorted_lectures_df['instructors'] = self.sorted_lectures_df['instructors'].apply(lambda x: x[0] if x else None)
+        # Convert dictionaries to Series for vectorized operations
+        gpa_series = pd.Series(self.gpa_dict)
+        rating_series = pd.Series(self.rating_dict)
+        difficulty_series = pd.Series(self.difficulty_dict)
+        would_take_again_series = pd.Series(self.would_take_again_dict)
+        prereq_freq_series = pd.Series(self.prereq_freq_dict)
+
+        # Initialize score and div as zero
+        self.sorted_lectures_df['score'] = 0
+        div = 0
+
+        # Update score and div based on sorting parameters
+        if "avg gpa" in self.sorting_parameters:
+            self.sorted_lectures_df['score'] += self.sorted_lectures_df['instructors'].map(gpa_series).fillna(2)
+            div += 1
+        if "avg rating" in self.sorting_parameters:
+            self.sorted_lectures_df['score'] += self.sorted_lectures_df['instructors'].map(rating_series).fillna(2.5)
+            div += 1
+        if "avg difficulty" in self.sorting_parameters:
+            self.sorted_lectures_df['score'] -= self.sorted_lectures_df['instructors'].map(difficulty_series).fillna(2.5)
+            div += 1
+        if "would take again" in self.sorting_parameters:
+            self.sorted_lectures_df['score'] += self.sorted_lectures_df['instructors'].map(would_take_again_series).fillna(50)
+            div += 1
+        if "prereq frequency" in self.sorting_parameters:
+            self.sorted_lectures_df['score'] += self.sorted_lectures_df['course'].map(prereq_freq_series).fillna(0)
+            div += 1
+
+        # Calculate average score
+        self.sorted_lectures_df['score'] /= div
+        # for index, row in self.sorted_lectures_df.iterrows():
+        #     course = row["course"]
+        #     instructor = row["instructors"][0]
+        #     score = 0
+        #     div = 0
+        #     if "avg gpa" in self.sorting_parameters and instructor in self.gpa_dict.keys():
+        #         score += self.gpa_dict[instructor] 
+        #         div += 1
+        #     if "avg rating" in self.sorting_parameters and instructor in self.rating_dict.keys():
+        #         score += self.rating_dict[instructor]
+        #         div += 1
+        #     if "avg difficulty" in self.sorting_parameters and instructor in self.difficulty_dict.keys():
+        #         score -= self.difficulty_dict[instructor]
+        #         div += 1
+        #     if "would take again" in self.sorting_parameters and instructor in self.would_take_again_dict.keys():
+        #         score += self.would_take_again_dict[instructor]
+        #         div += 1
+        #     if "prereq frequency" in self.sorting_parameters and course in self.prereq_freq_dict.keys():
+        #         score += self.prereq_freq_dict[course]
+        #         div += 1
+            # self.sorted_lectures_df.at[index, "score"] = score/div
     
     def sort(self) -> None:
         self.get_avg_score()
